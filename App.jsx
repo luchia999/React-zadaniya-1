0a285e60e27a54439f0d4fa18b4012c30e36805b
luchia999 <147498362+luchia999@users.noreply.github.com>
2024-08-25 18:02:40 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+export const App = () => {
+  return /*#__PURE__*/ _jsx("div", {
+    className: "App",
+    children: /*#__PURE__*/ _jsxs("header", {
+      className: "App-header",
+      children: [
+        /*#__PURE__*/ _jsx("img", {
+          src: logo,
+          className: "App-logo",
+          alt: "logo"
+        }),
+        /*#__PURE__*/ _jsxs("p", {
+          children: [
+            "Edit ",
+            /*#__PURE__*/ _jsx("code", {
+              children: "src/App.js"
+            }),
+            " and save to reload."
+          ]
+        }),
+        /*#__PURE__*/ _jsx("a", {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          children: "Learn React"
+        }),
+        /*#__PURE__*/ _jsx("p", {
+          children: "2024"
+        })
+      ]
+    })
+  });
+};
