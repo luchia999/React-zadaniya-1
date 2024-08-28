5f48aa34caee78de1013b3c5206afaa7e83e21c5
luchia999 <147498362+luchia999@users.noreply.github.com>
2024-08-28 15:33:27 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,32 +1,35 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 export const App = () => {
-  return /*#__PURE__*/ _jsx("div", {
+  return /*#__PURE__*/"React".createElement(
+	"div",
+	null,
+	 {
     className: "App",
-    children: /*#__PURE__*/ _jsxs("header", {
+    children: /*#__PURE__*/"React".createElement("header", {
       className: "App-header",
       children: [
-        /*#__PURE__*/ _jsx("img", {
-          src: logo,
+        /*#__PURE__*/ "React".createElement("img", {
+          src: "logo",
           className: "App-logo",
           alt: "logo"
         }),
-        /*#__PURE__*/ _jsxs("p", {
+        /*#__PURE__*/"React".createElement("p", {
           children: [
             "Edit ",
-            /*#__PURE__*/ _jsx("code", {
+            /*#__PURE__*/"React".createElement("code", {
               children: "src/App.js"
             }),
             " and save to reload."
           ]
         }),
-        /*#__PURE__*/ _jsx("a", {
+        /*#__PURE__*/"React".createElement("a", {
           className: "App-link",
           href: "https://reactjs.org",
           target: "_blank",
           rel: "noopener noreferrer",
           children: "Learn React"
         }),
-        /*#__PURE__*/ _jsx("p", {
+        /*#__PURE__*/"React".createElement("p", {
           children: "2024"
         })
       ]
